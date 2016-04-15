a25c8e34fcd0ab39bddf386b02413384b18c4cda
Smeily9PJ <siclebree.fairway@hotmail.fr>
2016-04-15 16:16:18 +0200

    test

--- FirstSolution/ITI2016.Dev/List.cs
+++ FirstSolution/ITI2016.Dev/List.cs
@@ -17,10 +17,10 @@ namespace ITI2016.Dev
             _array = new T[4];
         }
 
-        public List( params T[] args )
+        public List(params T[] args)
         {
             _array = (T[])args.Clone();
-            _count = _array.Length;
+            _count = args.Length;
         }
 
         public T this[int i]
